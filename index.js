4b9c5847d6fbf301f9b27bea21a1b15d5a329bfc
xLeYoUsSeF <youssef.gamer9za@gmail.com>
2024-10-30 01:08:45 +0300

    Update

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 let btn = document.querySelector(".generate");
 let serial = document.querySelector(".serial");
+let everyLi = document.querySelectorAll(".gallery li");
+let arrayLi = Array.from(everyLi);
 
-btn.onclick = function () {
-    let chars = "1234567890qwertyuioplkjhgfdsazxcvbnm";
-    let result = "";
-    for (let i = 0; i < 4; i++) {
-        result += chars[Math.floor(Math.random() * chars.length)];
+arrayLi.forEach((li) => {
+  li.addEventListener("click", function (e) {
+    arrayLi.forEach((li) => {
+      li.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+    if (e.currentTarget.classList.contains("active")) {
+      if (e.currentTarget.dataset.custom === "three") {
+        btn.onclick = function () {
+          let chars = "1234567890qwertyuioplkjhgfdsazxcvbnm";
+          let result = "";
+          for (let i = 0; i < 3; i++) {
+            result += chars[Math.floor(Math.random() * chars.length)];
+          }
+          serial.innerHTML = result;
+          serial.style.width = "100px";
+        };
+      } else if (e.currentTarget.dataset.custom === "four") {
+        btn.onclick = function () {
+            let chars = "1234567890qwertyuioplkjhgfdsazxcvbnm";
+            let result = "";
+            for (let i = 0; i < 4; i++) {
+              result += chars[Math.floor(Math.random() * chars.length)];
+            }
+            serial.innerHTML = result;
+            serial.style.width = "100px";
+          };
+      }
     }
-    serial.innerHTML = result;
-    serial.style.width = '100px'
-}
+  });
+});
